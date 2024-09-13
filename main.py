4d100b19eed928a36b3369237e7c67a1f7b4b888
rpyle <rpyle@guhsd.net>
2024-09-13 09:01:50 -0700

    updated leg_length

--- main.py
+++ main.py
@@ -6,14 +6,14 @@ painter = trtl.Turtle()
 painter.pensize(40)
 painter.circle(20)
 number_of_legs = 6
-y = 70
+leg_length = 70
 z = 380 / number_of_legs
 painter.pensize(5)
 n = 0
 while (n < number_of_legs):
   painter.goto(0,0)
   painter.setheading(z*n)
-  painter.forward(y)
+  painter.forward(leg_length)
   n = n + 1
 painter.hideturtle()
 wn = trtl.Screen()
